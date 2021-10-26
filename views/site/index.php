1b260970c1ed9faddee21b694be0a7ee2a30cea8
OlesyaSokolova <tsapa23@mail.ru>
2021-10-27 00:03:46 +0700

    created empty project

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Petroglyphs Gallery';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Hello, world!</h1>
+    </div>
+</div>
